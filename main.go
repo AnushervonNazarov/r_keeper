8e7f2d6cdd39baa7cbe0d31f346e7412bb3e8a3e
Anushervon <anushervonnazarov1@gmail.com>
2025-01-24 22:06:09 +0500

    refactoring

--- main.go
+++ main.go
@@ -31,57 +31,57 @@ import (
 func main() {
 
 	if err := godotenv.Load(".env"); err != nil {
-		log.Fatalf("Ошибка загрузки .env файла: %s", err)
+		log.Fatalf("Error loading .env file: %s", err)
 	}
 
 	if err := configs.ReadSettings(); err != nil {
-		log.Fatalf("Ошибка чтения настроек: %s", err)
+		log.Fatalf("Error reading settings: %s", err)
 	}
 
 	if err := logger.Init(); err != nil {
-		log.Fatalf("Ошибка инициализации логгера: %s", err)
+		log.Fatalf("Logger initialization error: %s", err)
 	}
 
 	if err := db.ConnectToDB(); err != nil {
-		log.Fatalf("Ошибка подключения к базе данных: %s", err)
+		log.Fatalf("Database connection error: %s", err)
 	}
 
 	if err := db.Migrate(); err != nil {
-		log.Fatalf("Ошибка миграции базы данных: %s", err)
+		log.Fatalf("Database migration error: %s", err)
 	}
 
 	mainServer := new(server.Server)
 	go func() {
 		if err := mainServer.Run(configs.AppSettings.AppParams.PortRun, controllers.RunRouts()); err != nil {
-			log.Fatalf("Ошибка при запуске HTTP сервера: %s", err)
+			log.Fatalf("Error starting HTTP server: %s", err)
 		}
 	}()
 
-	// Ожидание сигнала для завершения работы
+	// Waiting for signal to terminate
 	quit := make(chan os.Signal, 1)
 	signal.Notify(quit, syscall.SIGTERM, syscall.SIGINT)
 	<-quit
 
-	fmt.Printf("\nНачало завершения программ\n")
+	fmt.Printf("\nStart of program completion\n")
 
-	// Закрытие соединения с БД, если необходимо
+	// Close the connection to the database if necessary
 	if sqlDB, err := db.GetDBConn().DB(); err == nil {
 		if err := sqlDB.Close(); err != nil {
-			log.Fatalf("Ошибка при закрытии соединения с БД: %s", err)
+			log.Fatalf("Error closing connection to DB: %s", err)
 		}
 	} else {
-		log.Fatalf("Ошибка при получении *sql.DB из GORM: %s", err)
+		log.Fatalf("Error getting *sql.DB from GORM: %s", err)
 	}
-	fmt.Println("Соединение с БД успешно закрыто")
+	fmt.Println("The connection to the database was closed successfully")
 
-	// Используем контекст с тайм-аутом для завершения работы сервера
+	// Using a context with a timeout to shut down the server
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	if err := mainServer.Shutdown(ctx); err != nil {
-		log.Fatalf("Ошибка при завершении работы сервера: %s", err)
+		log.Fatalf("Error while shutting down the server: %s", err)
 	}
 
-	fmt.Println("HTTP-сервис успешно выключен")
-	fmt.Println("Конец завершения программы")
+	fmt.Println("HTTP service successfully shut down")
+	fmt.Println("End of program completion")
 }
